126201f53605fdfb470047052387e72c95531fe8
konynunez <156865892+konynunez@users.noreply.github.com>
2024-07-05 20:00:03 -0400

    edited about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,10 +5,17 @@ export default function About() {
   return (
     <div className="flex flex-col items-center mb-8">
       <header className="w-full">
-        <Image src={seriesImage} alt="Header Image" layout="responsive" className="w-full h-auto" />
+        <Image 
+          src={seriesImage} 
+          alt="Header Image" 
+          layout="responsive" 
+          width={1200} 
+          height={100} 
+          className="w-full h-auto" 
+        />
       </header>
-      <h1 className="text-4xl text-red-500 text-center mt-4">About Us</h1>
-      <p className="font-bold mt-10 text-center">Welcome to our List Management App. This is the About page.  In this app you can add, 
+      <h1 className="text-4xl text-indigo-500 text-center mt-4">About Us</h1>
+      <p className="font-bold mt-10 text-center">Welcome to our List Management App. This is the About page. In this app you can add, 
         edit, erase shows.</p>
     </div>
   );
